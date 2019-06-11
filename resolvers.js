1971db862a9ef8f4909b8259566dbf3d8b0d7c92
Ezar Enanda <XEEZAR@sig.com>
2019-06-11 15:45:10 +0700

    using squel query builder for easy filtering

--- resolvers.js
+++ resolvers.js
@@ -1,23 +1,108 @@
 const db = require("./database");
+const squel = require("squel");
 
 module.exports = {
-  cards: ({ search }) => {
-    let sql = `SELECT texts.name, texts.desc, datas.*, 
-      definitions.types.type, definitions.races.race, definitions.attributes.attribute
-      FROM datas 
-      JOIN texts ON datas.id = texts.id 
-      LEFT JOIN definitions.types ON datas.type = definitions.types.id 
-      LEFT JOIN definitions.races ON datas.race = definitions.races.id 
-      LEFT JOIN definitions.attributes ON datas.attribute = definitions.attributes.id`;
+  cards: ({ search, types, races, attributes }) => {
+    let sql = squel
+      .select()
+      .from("datas")
+      .join("texts", "texts", "datas.id = texts.id")
+      .left_join(
+        "definitions.types",
+        "types",
+        "datas.type = definitions.types.id"
+      )
+      .left_join(
+        "definitions.races",
+        "races",
+        "datas.race = definitions.races.id"
+      )
+      .left_join(
+        "definitions.attributes",
+        "attributes",
+        "datas.attribute = definitions.attributes.id"
+      );
 
-    let params = {};
     if (search) {
-      params["$search"] = `${search}%`;
-      sql += ` WHERE LOWER(texts.name) LIKE LOWER($search) OR LOWER(texts.desc) LIKE LOWER($search)`;
+      sql.where(
+        squel
+          .expr()
+          .or(`LOWER(texts.name) LIKE LOWER(?)`, `%${search}%`)
+          .or(`LOWER(texts.desc) LIKE LOWER(?)`, `%${search}%`)
+      );
     }
 
-    sql += `GROUP BY texts.name`;
+    if (types && types.length > 0) {
+      sql
+        .from(
+          squel
+            .select()
+            .from("definitions.types")
+            .field("SUM(id)", "sum")
+            .where(
+              "LOWER(type) IN ?",
+              squel.str(
+                types
+                  .map(function() {
+                    return "LOWER(?)";
+                  })
+                  .join(","),
+                ...types
+              )
+            ),
+          "type_filter"
+        )
+        .where("type_filter.sum & datas.type > 0");
+    }
 
-    return db.cardsDb.all(sql, params);
+    if (races && races.length > 0) {
+      sql
+        .from(
+          squel
+            .select()
+            .from("definitions.races")
+            .field("SUM(id)", "sum")
+            .where(
+              "LOWER(race) IN ?",
+              squel.str(
+                races
+                  .map(function() {
+                    return "LOWER(?)";
+                  })
+                  .join(","),
+                ...races
+              )
+            ),
+          "race_filter"
+        )
+        .where("race_filter.sum & datas.race > 0");
+    }
+
+    if (attributes && attributes.length > 0) {
+      sql
+        .from(
+          squel
+            .select()
+            .from("definitions.attributes")
+            .field("SUM(id)", "sum")
+            .where(
+              "UPPER(attribute) IN ?",
+              squel.str(
+                attributes
+                  .map(function() {
+                    return "UPPER(?)";
+                  })
+                  .join(","),
+                ...attributes
+              )
+            ),
+          "attribute_filter"
+        )
+        .where("attribute_filter.sum & datas.attribute > 0");
+    }
+
+    sql.group("texts.name"); // remove duplicate cards from different sets
+    // console.log(sql.toString());
+    return db.cardsDb.all(sql.toString());
   }
 };
